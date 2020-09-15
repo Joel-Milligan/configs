f8098927aac9a0ef39e601384adf71d8d2968cd2
Joel Milligan <22701252@student.uwa.edu.au>
2020-09-15 19:32:38 +0800

    Added a setup script

---
+++ initial_setup.sh
@@ -0,0 +1,15 @@
+# This is the list of commands I ran to set WSL 2 Ubuntu up
+
+# Prompted to enter a new UNIX username
+# Prompted to enter password
+
+sudo apt update
+# Enter password
+sudo apt upgrade -y
+rm .bashrc .bash_logout .profile
+git clone https://github.com/Joel-Milligan/dotfiles.git
+shopt -s dotglob
+mv ~/dotfiles/* ~
+rmdir dotfiles
+source .bashrc
+reload

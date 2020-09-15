a3c75e85f9bc7d94e8b3403ccfb7652fac4f4bdd
Joel-Milligan <52927756+Joel-Milligan@users.noreply.github.com>
2020-09-15 23:33:07 +0800

    Shebang and user home

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,15 +1,12 @@
+#!/bin/bash
 # This is the list of commands I ran to set WSL 2 Ubuntu up
 
-# Prompted to enter a new UNIX username
-# Prompted to enter password
-
 sudo apt update
-# Enter password
 sudo apt upgrade -y
 git clone https://github.com/Joel-Milligan/dotfiles.git
 rm .bashrc .bash_logout .profile
 shopt -s dotglob
-mv ~/dotfiles/* ~
+mv ./dotfiles/* .
 rmdir dotfiles
 source .bashrc
 echo "Completed Setup, may need to source .bashrc/restart"

872e657ef10948c724b118ef13e7e79ffe4eb4eb
Joel Milligan <joelmilligan24@gmail.com>
2023-05-13 14:08:21 +0800

    Add keymaps and remove unneccessary files

--- nvim/lua/custom/init.lua
+++
@@ -1,6 +0,0 @@
-require("opts")
-require("lsp")
-require("plugins")
-require("remap")
-require("treesitter")
-
--- nvim/lua/custom/remap.lua
+++
@@ -1,6 +0,0 @@
-vim.g.mapleader = " " -- Set the leader key to space
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- Quick way to go back to netrw
-
--- Telescope remaps
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {}) -- Find all files from current directory
